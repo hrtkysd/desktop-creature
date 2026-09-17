8fc2c25374d04121eaa67c5b5783c4366ef3936a
Hirotaka Yoshida <hirotaka.yoshida@avanade.com>
2026-09-17 16:53:47 +0900

    delete CAngle. use Angle namespace.

--- CreatureCore/Angle.h
+++ CreatureCore/Angle.h
@@ -4,18 +4,17 @@ namespace Creature
 {
     namespace Math
     {
-        constexpr float PI = 3.14159265358979323846f;
-        constexpr float TWO_PI = PI * 2.0f;
-
-        class CAngle
+        namespace Angle
         {
-        public:
-            static float Normalize(float angle)
+            constexpr float PI = 3.14159265358979323846f;
+            constexpr float TWO_PI = PI * 2.0f;
+
+            inline float Normalize(float angle)
             {
                 while (angle > PI) angle -= TWO_PI;
                 while (angle < -PI) angle += TWO_PI;
                 return angle;
             }
-        };
+        }
     }
 }
